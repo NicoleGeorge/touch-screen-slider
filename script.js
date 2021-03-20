d9049e20adea456183fad70e8b49ab4be78952fe
Nicole George <hello@nicolegeorge.me>
2021-03-20 21:50:55 +1030

    dearing vars, adding events

---
+++ script.js
@@ -0,0 +1,24 @@
+const slider = document.querySelector('.slider-container'),
+slides = Array.from(document.querySelectorAll('.slide'))
+
+let isDragging = false,
+startPosition = 0,
+currentTranslate = 0,
+prevTranslate = 0,
+animationID = 0,
+currentIndex = 0;
+
+slides.forEach((slide, index) => {
+  const slideImg = slide.querySelector('img');
+  slideImg.addEventListener('dragstart', (e) => e.preventDefault());
+  // TOUCH EVENTS
+  slide.addEventListener('touchstart', touchStart(index));
+  slide.addEventListener('touchend', touchEnd);
+  slide.addEventListener('touchmove', touchMove);
+  // MOUSE EVENTS
+  slide.addEventListener('mousedown', touchStart(index));
+  slide.addEventListener('mouseup', touchEnd);
+  slide.addEventListener('mouseleave', touchEnd);
+  slide.addEventListener('mousemove', touchMove);
+})
+
